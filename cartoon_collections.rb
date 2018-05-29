e99f79f3e70159c4114a24c0a88e25dda5858424
Mark Lawrence <lawrence.ml@gmail.com>
2018-05-29 20:49:38 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,5 +14,5 @@ end
 
 def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  foods.find {|cheese| cheese_types.include?(cheese)}
+  foods.find {|food| cheese_types.include?(food)}
 end
