59d9b7e5e89877d73c99eda8a54d1ffba184de6f
Mark Lawrence <lawrence.ml@gmail.com>
2018-05-29 19:49:21 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,9 @@ def summon_captain_planet(calls)
   calls.map! {|call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)
   # Your code here
+  calls.any? { |call| call.length > 4 }
 end
 
 def find_the_cheese# code an argument here
